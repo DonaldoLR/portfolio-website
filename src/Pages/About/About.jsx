9f69c8e6a06ae9dec64ea5a77519b2e085974895
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-02-12 17:47:42 -0500

    Updated the about me section page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 const About = () => {
-  return (
-    <section>
-      <div className='container'>
-        <p className='section-tag'>About</p>
-        <h1 className='section-heading'>About Me!</h1>
-        <div className='grid grid-twoCol'>
-          <div className='about-profile-image'></div>
-          {/* <img
+	return (
+		<section>
+			<div className='container'>
+				<p className='section-tag'>About</p>
+				<h1 className='section-heading'>About Me!</h1>
+				<div className='grid grid-twoCol'>
+					<div className='about-profile-image'></div>
+					{/* <img
             src='https://marketifythemes.com/html/tokyo/img/slider/1.jpg'
             alt='Portrait'
           ></img> */}
-          <div className='about-info-container'>
-            <h1>Donaldo Lara Reyes</h1>
-            <p>
-              Hello, I am a creative photographer based in New York and happy to
-              travel all over Europe to capture your big day in candid and
-              authentic photos. I will create a lasting memory of the people.
-            </p>
-            <NavLink className='button' to='/Contact'>
-              Contact Me
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+					<div className='about-info-container'>
+						<h1>Donaldo Lara Reyes</h1>
+						<p>
+							Hello, I am a detail oriented Software Engineer based in North
+							Carolina. I enjoy helping others, build things that make my job
+							easier, and efficient. On my free time, I like taking Udemy
+							(online courses in general) courses, currently I am taking "Github
+							& Git" by Colt Steele.
+						</p>
+						<NavLink className='button' to='/Contact'>
+							Contact Me
+						</NavLink>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
