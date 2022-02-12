44d5fe22a3ee72b6aca337e55f67c6f2da0b89e1
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-02-12 17:56:21 -0500

    remove NavLink import line to remove error in Netlify

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import avatar from '../../Assets/avatar.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 const Home = () => {
 	return (
 		<section className='landing'>
