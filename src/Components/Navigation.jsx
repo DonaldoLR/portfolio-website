6f8d971111c3956b10036b0bf0c0c560df145056
Donaldo Lara-Reyes <bonelessdarkmatter_code@protonmail.com>
2022-02-12 17:39:29 -0500

    Removed portfolio section of the website, updated home page opening message

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,61 +2,60 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 // import Logo from '../Assets/Logo With Name and Tagline.svg';
 const Navigation = () => {
-  const [displayMenu, setdisplayMenu] = useState(false);
-  const toggleMenu = () => {
-    setdisplayMenu(!displayMenu);
-  };
-  return (
-    <header>
-      <nav>
-        <h1 className='logo'>
-          {/* <img src={Logo} alt='Logo' /> */}
-          DONALDO
-        </h1>
-        <ul className={displayMenu ? 'open nav-list' : 'nav-list'}>
-          <li className='nav-list-item'>
-            <NavLink exact to='/' className='nav-link' onClick={toggleMenu}>
-              Home
-            </NavLink>
-          </li>
-          <li className='nav-list-item'>
+	const [displayMenu, setdisplayMenu] = useState(false);
+	const toggleMenu = () => {
+		setdisplayMenu(!displayMenu);
+	};
+	return (
+		<header>
+			<nav>
+				<h1 className='logo'>
+					{/* <img src={Logo} alt='Logo' /> */}
+					DONALDO
+				</h1>
+				<ul className={displayMenu ? 'open nav-list' : 'nav-list'}>
+					<li className='nav-list-item'>
+						<NavLink exact to='/' className='nav-link' onClick={toggleMenu}>
+							Home
+						</NavLink>
+					</li>
+					{/* <li className='nav-list-item'>
             <NavLink to='/Portfolio' className='nav-link' onClick={toggleMenu}>
               Portfolio
             </NavLink>
-          </li>
-          <li className='nav-list-item'>
-            <NavLink to='/About' className='nav-link' onClick={toggleMenu}>
-              About
-            </NavLink>
-          </li>
-          {/* <li className='nav-list-item'>
+          </li> */}
+					<li className='nav-list-item'>
+						<NavLink to='/About' className='nav-link' onClick={toggleMenu}>
+							About
+						</NavLink>
+					</li>
+					{/* <li className='nav-list-item'>
             <NavLink to='/Blog' className='nav-link' onClick={toggleMenu}>
               Blog
             </NavLink>
           </li> */}
-          <li className='nav-list-item'>
-            <NavLink to='/Contact' className='nav-link' onClick={toggleMenu}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-        <button
-          className={
-            displayMenu
-              ? 'hamburger hamburger--spin is-active'
-              : 'hamburger hamburger--spin'
-          }
-          type='button'
-          onClick={toggleMenu}
-        >
-          <span className='hamburger-box'>
-            <span className='hamburger-inner'></span>
-          </span>
-        </button>
-        <p className='nav-footer'>&copy; 2021 DONALDO</p>
-      </nav>
-    </header>
-  );
+					<li className='nav-list-item'>
+						<NavLink to='/Contact' className='nav-link' onClick={toggleMenu}>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+				<button
+					className={
+						displayMenu
+							? 'hamburger hamburger--spin is-active'
+							: 'hamburger hamburger--spin'
+					}
+					type='button'
+					onClick={toggleMenu}>
+					<span className='hamburger-box'>
+						<span className='hamburger-inner'></span>
+					</span>
+				</button>
+				<p className='nav-footer'>&copy; 2021 DONALDO</p>
+			</nav>
+		</header>
+	);
 };
 
 export default Navigation;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,58 +3,56 @@ import avatar from '../../Assets/avatar.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink } from 'react-router-dom';
 const Home = () => {
-  return (
-    <section className='landing'>
-      <div className='grid grid-twoCol landing-hero container'>
-        <div className='landing-image'>
-          <img alt='Donaldo Avatar' src={avatar} width='100%' />
-        </div>
-        <div className='landing-content'>
-          <h1>Donaldo Lara-Reyes</h1>
-          <p>
-            Freelance Website Developer located in North Carolina. Looking for
-            businesses who would like to increase their online presence.
-          </p>
-          <NavLink to='/Portfolio' className='button'>
-            <span>See my work</span>
-            {/* <FontAwesomeIcon icon='arrow-right' size='2x' /> */}
-          </NavLink>
-          <ul>
-            <li>
-              <a
-                className='social-twitter'
-                href='https://twitter.com/DonaldoLR'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <FontAwesomeIcon icon={['fab', 'twitter']} size='2x' />
-              </a>
-            </li>
-            <li>
-              <a
-                className='social-linkedin'
-                href='https://www.linkedin.com/in/donaldolr/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <FontAwesomeIcon icon={['fab', 'linkedin-in']} size='2x' />
-              </a>
-            </li>
-            <li>
-              <a
-                className='social-github'
-                href='https://github.com/DonaldoLR'
-                target='_blank'
-                rel='noreferrer'
-              >
-                <FontAwesomeIcon icon={['fab', 'github']} size='2x' />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className='landing'>
+			<div className='grid grid-twoCol landing-hero container'>
+				<div className='landing-image'>
+					<img alt='Donaldo Avatar' src={avatar} width='100%' />
+				</div>
+				<div className='landing-content'>
+					<h1>Donaldo Lara-Reyes</h1>
+					<p>
+						Software Engineer located in North Carolina. Looking to help any
+						company, community, organizaition build awesome ideas. I specialize
+						in Front-End Website Development but down to learn something new.
+					</p>
+					<NavLink to='/Portfolio' className='button'>
+						<span>See my work</span>
+						{/* <FontAwesomeIcon icon='arrow-right' size='2x' /> */}
+					</NavLink>
+					<ul>
+						<li>
+							<a
+								className='social-twitter'
+								href='https://twitter.com/DonaldoLR'
+								target='_blank'
+								rel='noreferrer'>
+								<FontAwesomeIcon icon={['fab', 'twitter']} size='2x' />
+							</a>
+						</li>
+						<li>
+							<a
+								className='social-linkedin'
+								href='https://www.linkedin.com/in/donaldolr/'
+								target='_blank'
+								rel='noreferrer'>
+								<FontAwesomeIcon icon={['fab', 'linkedin-in']} size='2x' />
+							</a>
+						</li>
+						<li>
+							<a
+								className='social-github'
+								href='https://github.com/DonaldoLR'
+								target='_blank'
+								rel='noreferrer'>
+								<FontAwesomeIcon icon={['fab', 'github']} size='2x' />
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
